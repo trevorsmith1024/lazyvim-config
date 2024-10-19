7860d63bc12082c5803d7eef491304bc161010d9
Trevor <>
2024-10-19 02:21:58 -0500

    standard tsserver appears to work

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,7 +1,9 @@
 return {
-  "tummetott/reticle.nvim",
-  event = "VeryLazy", -- optionally lazy load the plugin
-  opts = {
-    -- add options here if you wish to override the default settings
+  {
+    "tummetott/reticle.nvim",
+    event = "VeryLazy", -- optionally lazy load the plugin
+    opts = {
+      -- add options here if you wish to override the default settings
+    },
   },
 }
