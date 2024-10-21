8d3c1ea8f2779f5d3dcd5fcfe4f23b3ce128e73e
Trevor <>
2024-10-20 22:45:56 -0500

    instructions

--- otherConfigs/.wezterm.lua
+++ otherConfigs/.wezterm.lua
@@ -1,10 +1,12 @@
+-- move this to ~ i.e. (~/.wezterm.lua)
+
 local wezterm = require("wezterm")
 local config = {}
 config.keys = {
-	{
-		key = "k",
-		mods = "CMD",
-		action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
-	},
+  {
+    key = "k",
+    mods = "CMD",
+    action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
+  },
 }
 return config
